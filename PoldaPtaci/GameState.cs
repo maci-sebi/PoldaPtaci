16caecc0202e8f19f42a9be4be43c14fd17286fd
just seki <206236430+maci-sebi@users.noreply.github.com>
2026-05-23 23:10:52 +0100

    Add files via upload

---
+++ PoldaPtaci/GameState.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PoldaPtaci
+{
+    public class GameState
+    {
+        // aktualni scena
+        public string CurrentScene { get; set; } = "Menu";
+
+        // inventar
+        public List<string> Inventory { get; set; } = new List<string>();
+
+        // predmety na zemi
+        public bool GlassOnGround { get; set; } = true;
+        public bool FeatherOnGround { get; set; } = true;
+        public bool BranchesOnGround { get; set; } = true;
+        public bool LogOnGround { get; set; } = true;
+        public bool MossOnGround { get; set; } = true;
+
+        // questy
+        public bool HasCompass { get; set; } = false;      // spojeni sklo + pirko
+        public bool CarolPassed { get; set; } = false;     // pustil nas Carol
+        public bool JohnPassed { get; set; } = false;      // dal John info
+        public bool OwlGaveBerries { get; set; } = false;  // mame bobule
+        public bool NoirGaveEggs { get; set; } = false;    // dal vajicka
+        public bool HasNestWithEggs { get; set; } = false; // mame hnizdo
+        public bool GameFinished { get; set; } = false;    // konec hry 
+    }
+}
